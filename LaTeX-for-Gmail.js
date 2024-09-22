072894c30fcd572676bd7e76033f82e91de98a2d
LoganJFisher <63993409+LoganJFisher@users.noreply.github.com>
2024-09-22 14:39:14 -0400

    Update LaTeX-for-Gmail.js

--- LaTeX-for-Gmail.js
+++ LaTeX-for-Gmail.js
@@ -5,11 +5,11 @@
 // @grant       GM_registerMenuCommand
 // @grant       GM_addElement
 // @require     https://cdn.jsdelivr.net/npm/katex@0.16.11/dist/katex.min.js
-// @version     4.1.6 //First number for major changes, second for minor functionality improvements, third for bug fixes.
+// @version     4.1.7 //First number for major changes, second for minor functionality improvements, third for bug fixes.
 // @author      Logan J. Fisher & GTK & MistralMireille
 // @description Adds a button to Gmail which toggles LaTeX rendering using traditional LaTeX and TeXTheWorld delimiters
-// @downloadURL https://github.com/LoganJFisher/LaTeX-for-Gmail/raw/master/LaTeX-for-Gmail.user.js
-// @updateURL https://github.com/LoganJFisher/LaTeX-for-Gmail/raw/master/LaTeX-for-Gmail.user.js
+// @downloadURL https://raw.githubusercontent.com/LoganJFisher/LaTeX-for-Gmail/refs/heads/main/LaTeX-for-Gmail.js
+// @updateURL https://raw.githubusercontent.com/LoganJFisher/LaTeX-for-Gmail/refs/heads/main/LaTeX-for-Gmail.js
 // @noframes
 // ==/UserScript==
 
