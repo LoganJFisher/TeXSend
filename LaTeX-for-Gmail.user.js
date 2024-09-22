2f3346dba6634238188c277a29d66d4ae92342dc
LoganJFisher <63993409+LoganJFisher@users.noreply.github.com>
2024-09-22 15:19:20 -0400

    Update and rename LaTeX-for-Gmail.js to LaTeX-for-Gmail.user.js
    user.js

--- LaTeX-for-Gmail.user.js
+++ LaTeX-for-Gmail.user.js
@@ -1,17 +1,18 @@
 // ==UserScript==
-// @name        LaTeX for Gmail
-// @version     4.2.5
-//              First number for major changes, second for minor functionality improvements, third for bug fixes.
-// @description Adds a button to Gmail which toggles LaTeX rendering using traditional LaTeX and TeXTheWorld delimiters
-// @author      Logan J. Fisher & GTK & MistralMireille
-// @downloadURL https://raw.githubusercontent.com/LoganJFisher/LaTeX-for-Gmail/refs/heads/main/LaTeX-for-Gmail.user.js
-// @updateURL https://raw.githubusercontent.com/LoganJFisher/LaTeX-for-Gmail/refs/heads/main/LaTeX-for-Gmail.user.js
-// @supportURL  https://github.com/LoganJFisher/LaTeX-for-Gmail/issues
-// @namespace   Violentmonkey Scripts
-// @match       https://mail.google.com/mail/*
-// @grant       GM_registerMenuCommand
-// @grant       GM_addElement
-// @require     https://cdn.jsdelivr.net/npm/katex@0.16.11/dist/katex.min.js
+// @name            LaTeX for Gmail
+// @version         4.2.5
+//                  First number for major changes, second for minor functionality improvements, third for bug fixes.
+// @description     Adds a button to Gmail which toggles LaTeX rendering using traditional LaTeX and TeXTheWorld delimiters
+// @author          Logan J. Fisher & GTK & MistralMireille
+// @license         MIT
+// @downloadURL     https://raw.githubusercontent.com/LoganJFisher/LaTeX-for-Gmail/refs/heads/main/LaTeX-for-Gmail.user.js
+// @updateURL       https://raw.githubusercontent.com/LoganJFisher/LaTeX-for-Gmail/refs/heads/main/LaTeX-for-Gmail.user.js
+// @supportURL      https://github.com/LoganJFisher/LaTeX-for-Gmail/issues
+// @namespace       Violentmonkey Scripts
+// @match           https://mail.google.com/mail/*
+// @grant           GM_registerMenuCommand
+// @grant           GM_addElement
+// @require         https://cdn.jsdelivr.net/npm/katex@0.16.11/dist/katex.min.js
 
 // @noframes
 // ==/UserScript==
