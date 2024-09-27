11f52b4152a1e388fc22be20b6fb0ae640240504
LoganJFisher <63993409+LoganJFisher@users.noreply.github.com>
2024-09-27 18:24:48 -0400

    Update LaTeX-for-Gmail.user.js
    Moved banner for disabled edit with compile on in draft to top

--- LaTeX-for-Gmail.user.js
+++ LaTeX-for-Gmail.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name            LaTeX for Gmail
-// @version         5.7.0
+// @version         5.7.1
 // @description     Adds a button to Gmail which toggles LaTeX compiling
 // @author          Logan J. Fisher & GTK & MistralMireille
 // @license         MIT
@@ -17,9 +17,9 @@
 // @require         https://cdn.jsdelivr.net/npm/katex@0.16.11/dist/contrib/mhchem.min.js
 // @require         https://cdn.jsdelivr.net/npm/katex@0.16.11/dist/contrib/copy-tex.min.js
 // ==/UserScript==
-
+ 
 /* globals katex */
-
+ 
 const selectors = {
     topBar: 'div#\\:4',
     moveButton: 'div#\\:4 div[title="Move to"]',
@@ -31,7 +31,7 @@ const selectors = {
     sendButton: 'div[role=button][aria-label^=Send]',
     lockerButton: 'td:has(> div > div[command=locker])',
 }
-
+ 
 const DELIMITERS = [
     {left: '[(;' , right: ';)]' , display: true, includeDelimiter: false},
     {left: '\\[' , right: '\\]' , display: true, includeDelimiter: false},
@@ -39,7 +39,7 @@ const DELIMITERS = [
     {left: '\\(' , right: '\\)' , display: false, includeDelimiter: false},
     {left: '\\begin{displaymath}' , right: '\\end{displaymath}' , display: true, includeDelimiter: false},
     {left: '\\begin{math}' , right: '\\end{math}', display: false, includeDelimiter: false},
-
+ 
     {left: '\\begin{align}' , right: '\\end{align}', display: true, includeDelimiter: true},
     {left: '\\begin{align*}' , right: '\\end{align*}', display: true, includeDelimiter: true},
     {left: '\\begin{aligned}' , right: '\\end{aligned}', display: true, includeDelimiter: true},
@@ -72,9 +72,9 @@ const DELIMITERS = [
     {left: '\\begin{vmatrix}' , right: '\\end{vmatrix}', display: true, includeDelimiter: true},
     {left: '\\begin{vmatrix*}' , right: '\\end{vmatrix*}', display: true, includeDelimiter: true},
 ]
-
+ 
 const REGEX = buildRegex(DELIMITERS);
-
+ 
 function buildRegex(delims) {
     const escape = string => string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
     const expressions = delims.map( d => {
@@ -82,28 +82,28 @@ function buildRegex(delims) {
         const exp = d.includeDelimiter ? `(?<tex>${escape(d.left)}.+?${escape(d.right)})${display}` : `${escape(d.left)}(?<tex>.+?)${escape(d.right)}${display}`;
         return exp;
     })
-
+ 
     return new RegExp(expressions.join('|'), 'gs');
 }
-
+ 
 function renderLatex(html) {
     html = html.replace(/<wbr>/gs, '').replace(/&nbsp;/gs, ' '); // fixes parsing of long expressions (GMAIL inserts <wbr> tags for some reason) & removes white spaces after delimiters
     const div = document.createElement('div');
-
+ 
     html = html.replace(REGEX, function() {
         const groups = arguments[arguments.length - 1];
         const display = groups.d !== undefined;
         div.innerHTML = groups.tex;
         return katex.renderToString(div.textContent, {throwOnError: false, displayMode: display, trust: true, strict: false})
     })
-
+ 
     return html;
 }
-
+ 
 function updateLatex(messageList, state) {
     messageList.forEach(message => {
         if (state === message.rendered) return;
-
+ 
         if (state && !message.rendered) {
             message.oldHTML = message.innerHTML;
             message.innerHTML = renderLatex(message.innerHTML);
@@ -114,40 +114,40 @@ function updateLatex(messageList, state) {
         }
     });
 }
-
+ 
 // ===================================================================================================
 // MESSAGES
 // ===================================================================================================
-
+ 
 let MESSAGES_TOGGLE = true;
-
+ 
 function addMessageToggleButton() {
     const moveBtn = document.querySelector(selectors.moveButton);
     if (!moveBtn) return;
-
+ 
     const latexButton = GM_addElement(moveBtn.parentElement, 'div', {
         id: 'latex_toggle_message_button',
         role: 'button',
         'data-tooltip': 'Toggle LaTeX',
     });
-
+ 
     const logoDiv = GM_addElement(latexButton, 'div', {
         class: 'asa',
         style: 'width: 20px; height: 20px; display: inline-flex; align-items: end',
     });
-
+ 
     logoDiv.innerHTML = katex.renderToString('\\footnotesize \\TeX', {throwOnError: false});
-
+ 
     latexButton.addEventListener('click', toggleMessages);
     latexButton.addEventListener('mouseover', () => latexButton.classList.add('T-I-JW'));
     latexButton.addEventListener('mouseout', () => latexButton.classList.remove('T-I-JW'));
 }
-
-
+ 
+ 
 function observeMessages() {
     const messageList = document.querySelector(selectors.messageList);
     if (!messageList) return;
-
+ 
     const messages = messageList.querySelectorAll('div[role=listitem]');
     const observer = new MutationObserver( () => {
         refreshMessages();
@@ -155,21 +155,21 @@ function observeMessages() {
     });
     messages.forEach( msg => observer.observe(msg, {attributes: true, attributeFilter: ["aria-expanded", "class"]}) );
 }
-
+ 
 function refreshMessages() {
     const list = document.querySelectorAll(selectors.messageBody);
     updateLatex(list, MESSAGES_TOGGLE);
 }
-
+ 
 function toggleMessages() {
     MESSAGES_TOGGLE = !MESSAGES_TOGGLE;
     refreshMessages();
 }
-
+ 
 // ===================================================================================================
 // DRAFTS
 // ===================================================================================================
-
+ 
 function processDrafts(container) {
     const drafts = container.querySelectorAll(selectors.draftRegion);
     drafts.forEach( draft => {
@@ -180,7 +180,7 @@ function processDrafts(container) {
         draft.processed = true;
     })
 }
-
+ 
 function addDraftToggleButton(draft) {
     const buttonContainer = draft.querySelector(selectors.lockerButton);
     if (!buttonContainer) return;
@@ -190,39 +190,41 @@ function addDraftToggleButton(draft) {
         role: 'button',
         'data-tooltip': 'Toggle LaTeX',
     });
-
+ 
     button.innerHTML = katex.renderToString('\\scriptsize \\TeX', {throwOnError: false});
-
+ 
     const draftBody = draft.querySelector(selectors.draftBody);
     button.addEventListener('click', () => {
         updateLatex([draftBody], !draftBody.rendered)
         draftBody.setAttribute('contenteditable', !draftBody.rendered);
         draft.bannerDiv.style.visibility = draftBody.rendered ? 'visible': 'hidden';
+        draft.bannerDiv.style.marginBottom = draftBody.rendered ? '0px': '-30px';
     });
 }
-
+ 
 function addBanner(draft) {
-    const draftBody = draft.querySelector(selectors.draftBody);
-    const bannerDiv = GM_addElement(draftBody.closest('table').closest('td > div'), 'div', {
+    const parent = draft.querySelector('td:has(> form)');
+    const bannerDiv = GM_addElement(document.body, 'div', {
         textContent: 'Disable LaTeX to edit draft',
         id: 'latex_draft_banner',
     });
-
+ 
+    parent.insertBefore(bannerDiv, parent.children[parent.children.length - 1]);
     draft.bannerDiv = bannerDiv;
 }
-
+ 
 function attachSendListener(draft) {
     const draftBody = draft.querySelector(selectors.draftBody);
     const sendButton = draft.querySelector(selectors.sendButton);
-
+ 
     sendButton.addEventListener('click', () => updateLatex([draftBody], false), true);
 }
-
-
+ 
+ 
 // ===================================================================================================
 // UTILS
 // ===================================================================================================
-
+ 
 function waitForElement(queryString, interval=100, maxTries=100) {
     let count = 0;
     return new Promise((resolve, reject) => {
@@ -240,8 +242,8 @@ function waitForElement(queryString, interval=100, maxTries=100) {
         }, interval);
     });
 }
-
-
+ 
+ 
 function addShortcuts() {
     document.addEventListener('keydown', (event) => {
         if (event.ctrlKey && event.altKey && event.keyCode === 76) { //'L'
@@ -256,28 +258,28 @@ function addShortcuts() {
         }
     });
 }
-
+ 
 function addStyles() {
     GM_addElement('link', {
         rel: "stylesheet",
         href: "https://cdn.jsdelivr.net/npm/katex@0.16.11/dist/katex.css"
     });
-
+ 
     GM_addStyle(`
         .katex-display {
             max-width: 99%;
         }
-
+ 
         #\\:1 [role=list] > [role=listitem] {
             counter-reset: katexEqnNo;
         }
-
+ 
         #latex_toggle_message_button {
             cursor: pointer;
             margin: 0 16px 0 12px;
             color: var(--gm3-sys-color-on-surface);
         }
-
+ 
         #latex_toggle_draft_button {
             user-select: none;
             width: 20px;
@@ -286,7 +288,7 @@ function addStyles() {
             align-items: flex-end;
             margin: 4px 8px 4px -4px;
         }
-
+ 
         #latex_draft_banner {
             visibility: hidden;
             background-color: rgb(255, 85, 85);
@@ -297,14 +299,15 @@ function addStyles() {
             padding: 5px 50px;
             position: sticky;
             bottom: 0;
+            margin-bottom: -30px;
         }
     `);
 }
-
+ 
 // ===================================================================================================
 // MAIN
 // ===================================================================================================
-
+ 
 function init() {
     waitForElement(selectors.topBar).then(topbar => {
         const observer = new MutationObserver( () => {
@@ -314,7 +317,7 @@ function init() {
         });
         observer.observe(topbar, {attributes: false, childList: true});
     });
-
+ 
     waitForElement(selectors.draftsContainer).then(draftsContainer => {
         const observer = new MutationObserver( () => {
             processDrafts(draftsContainer);
@@ -322,22 +325,22 @@ function init() {
         observer.observe(draftsContainer, {attributes: true});
     });
 }
-
+ 
 function main() {
     if (window.trustedTypes && window.trustedTypes.createPolicy && !window.trustedTypes.defaultPolicy) {
         window.trustedTypes.createPolicy('default', {
             createHTML: string => string
         });
     }
-
+ 
     addStyles();
     addShortcuts();
     GM_registerMenuCommand('Toggle LaTeX', toggleMessages);
     init();
 }
-
+ 
 main();
-
+ 
 // Legal:
 // This userscript, LaTeX for Gmail, is an independent project and is not affiliated with, endorsed, sponsored, or supported by Google LLC, Alphabet Inc., or any of their subsidiaries. The aforementioned entities are not responsible for any issues, damages, or consequences arising from the use of this userscript.
 // By using this userscript, you acknowledge that you are doing so at your own risk and agree to hold Google LLC, Alphabet Inc., and their respective affiliates harmless from any claims, losses, or damages arising from your use of this userscript.
