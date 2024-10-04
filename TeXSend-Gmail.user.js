6626770c73cb463899723e84fb5396639929ebc8
LoganJFisher <63993409+LoganJFisher@users.noreply.github.com>
2024-10-03 21:26:38 -0400

    Update TeXSend-Gmail.user.js
    Name correction

--- TeXSend-Gmail.user.js
+++ TeXSend-Gmail.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
-// @name            TeXSend - Gmail
-// @version         6.1.10
+// @name            TeXSend-Gmail
+// @version         6.1.11
 // @description     Adds a button to Gmail which toggles LaTeX compiling
 // @author          Logan J. Fisher & GTK & MistralMireille
 // @license         MIT
@@ -433,7 +433,7 @@ function main() {
 main();
 
 // Legal:
-// This userscript, TeXSend - Gmail, is an independent project and is not affiliated with, endorsed, sponsored, or supported by Google LLC, Alphabet Inc., or any of their subsidiaries. The aforementioned entities are not responsible for any issues, damages, or consequences arising from the use of this userscript.
+// This userscript, TeXSend-Gmail, is an independent project and is not affiliated with, endorsed, sponsored, or supported by Google LLC, Alphabet Inc., or any of their subsidiaries. The aforementioned entities are not responsible for any issues, damages, or consequences arising from the use of this userscript.
 // By using this userscript, you acknowledge that you are doing so at your own risk and agree to hold Google LLC, Alphabet Inc., and their respective affiliates harmless from any claims, losses, or damages arising from your use of this userscript.
 // Google LLC reserves the right to request that the distribution of this userscript be ceased, or that the userscript be altered, if it violates Google's terms of service, policies, or guidelines, or if it causes harm to Google's reputation, user experience, or data privacy.
 // The MIT license under which this userscript is distributed can be viewed [here](https://github.com/LoganJFisher/TeXSend?tab=MIT-1-ov-file).
